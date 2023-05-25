b1d811ecc8d2c1184bf55727c0e5fa7b63420837
RomuloAP00 <romuloantoniopereira@outlook.com>
2023-05-25 10:34:56 -0300

    Retomada

---
+++ src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    // Atributos da classe
+    public String nome;
+    public String sobrenome;
+    // métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    }
+}
